2f4a802ad8fb5fe7e25b6bbafb65906afd6c9f44
Sota3110 <144866058+Soa1208@users.noreply.github.com>
2024-01-18 08:09:42 +0900

    8-books・usersコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   root to: "homes#top"
   devise_for :users
 end
