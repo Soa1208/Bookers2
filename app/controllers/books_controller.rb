e0b9aea0d5c043d074f7fff81370581ac6827073
Sota3110 <144866058+Soa1208@users.noreply.github.com>
2024-01-23 17:22:35 +0900

    中間

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,44 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  ｓｓ
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to users_path
+  end 
+  
   def index
+    @books = Book.includes(:user).all
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
-  def edit
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+  
 end
