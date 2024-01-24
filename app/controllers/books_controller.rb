69c0208d3ce33dc5c13d56c673173bbfb1d9eb17
Sota3110 <144866058+Soa1208@users.noreply.github.com>
2024-01-24 20:27:31 +0900

    User Info訂正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-  
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -16,10 +12,14 @@ class BooksController < ApplicationController
   end 
   
   def index
+    @user = current_user
+    @book = Book.new
     @books = Book.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @book_new = Book.new
     @book = Book.find(params[:id])
   end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
   def index
-     @users = User.all
+    @book = Book.new
+    @user = current_user
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
-  end  
+    @book = Book.new
+  end
   
   def edit
     @user = User.find(params[:id])
